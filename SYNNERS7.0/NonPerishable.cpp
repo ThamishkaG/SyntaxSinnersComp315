3c9f6c772d6d964f92599930501531aabad2a5ad
Nkanyiso-Msomi <nkanyisomsomi77@gmail.com>
2026-04-05 09:55:47 +0200

    Update NonPerishable.cpp

--- SYNNERS7.0/NonPerishable.cpp
+++ SYNNERS7.0/NonPerishable.cpp
@@ -1,11 +1,36 @@
 #include "NonPerishable.h"
 //edited by Cebile Nxumalo
-NonPerishable::NonPerishable(int productID,string name,double price,int quantity,double taxRate,double discountRate)
-      :Product(productID, name, price, taxRate, discountRate)
-{
-    //ctor
+//contructor of non perisharable
+NonPerishable::NonPerishable(int id, const std::string& productName, double price,
+                             double tax, int qty)
+    : Product(id, productName, price, qty),
+      taxRate(tax),{}
+
+//distructor of non perisharable
+NonPerishable::~NonPerishable() {
+
 }
 
+double NonPerishable::calculateFinalPrice() const {
+    double price = getPrice()+getPrice()*taxRate() ;
+
+
+    if (discountPrice!=0) {
+        price = getPrice()+getPrice()*taxRate() - discountPrice ;
+    }
+}
+
+//return tax rate
+double NonPerishable::getTaxRate() const {
+    return taxRate;
+}
+
+//update tax rate
+void NonPerishable::setTaxRate(double tax) {
+
+   taxRate = tax;
+
+}
 NonPerishable::~NonPerishable()
 {
     //dtor
