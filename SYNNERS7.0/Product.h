4cdce1a95a82a2f811a9188de890c9c7a7e4f7e1
Nkanyiso-Msomi <nkanyisomsomi77@gmail.com>
2026-04-05 10:00:10 +0200

    Update Product.h

--- SYNNERS7.0/Product.h
+++ SYNNERS7.0/Product.h
@@ -47,62 +47,6 @@ class Product
 
 
 };
-class Perishable : public Product {
-private:
-    std::string expiryDate; 
-    double discountPercentage; 
-    
-public:
-   // Constructor: Initializes Perishable with given values
-    Perishable(int id, const std::string& productName, double price, 
-               const std::string& expiry, double discount, int qty );
-    
-    // distructor
-    ~Perishable();
-    
-    
-    double calculateFinalPrice() const override;
-    
-    
-    
-    // return the expiry date of a perishable product
-    std::string getExpiryDate() const;
-    
-    //update the expirery date 
-    void setExpiryDate(const std::string& expiry);
-    
-    //set a discount of a product 
-    void setDiscount(double discount);
-    
-  
-};
 
-
-class NonPerishable : public Product {
-private:
-    
-  
-    
-public:
-   // Constructor: Initializes a perishable product with given values 
-    NonPerishable(int id, const std::string& productName, double price, 
-                  double tax ,  int qty );
-    
-    
-    ~NonPerishable();
-    
-    
-    double calculateFinalPrice() const override;
-    
-    
-   
-    
-    
-    double getTaxRate() const;
-    void setTaxRate(double tax);
-   
-    
-    
-};
 
 #endif // PRODUCT_H
