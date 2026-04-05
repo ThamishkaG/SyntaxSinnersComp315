7409dd229d169adc0e54c464a2c6e9f34bf558de
Nkanyiso-Msomi <nkanyisomsomi77@gmail.com>
2026-04-05 09:52:58 +0200

    Update Perishable.cpp

--- SYNNERS7.0/Perishable.cpp
+++ SYNNERS7.0/Perishable.cpp
@@ -1,12 +1,31 @@
 #include "Perishable.h"
 
-Perishable::Perishable(int productID,string name,double price,int quantity,double taxRate,double discountRate,string expiryDate)
-    :Product(productID, name, price, discountRate, taxRate), expiryDate(expiryDate)
-{
-    //ctor
+Perishable::Perishable(int id, const std::string& productName, double price,
+                       const std::string& expiry, double discount, int qty)
+    : Product(id, productName, price, qty),
+
+      expiryDate(expiry),
+      discountPrice(discount) {}
 }
 
-Perishable::~Perishable()
-{
-    //dtor
+//distructor
+Perishable::~Perishable() {}
+
+//return final price icluding tax and discount
+double Perishable::calculateFinalPrice() const {
+    double price = getPrice()+getPrice()*taxRate() ;
+
+
+    if (discountPrice!=0) {
+        price = getPrice()+getPrice()*taxRate() - discountPrice ;
+    }
+}
+// return expiry date
+std::string Perishable::getExpiryDate() const {
+    return expiryDate;
+}
+
+//update expiry date
+void Perishable::setExpiryDate(const std::string& expiry) {
+    expiryDate = expiry;
 }
