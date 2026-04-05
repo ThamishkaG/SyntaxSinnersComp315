fe4f9958fdc6438cd2466d406167b5f8e8cb6811
Nkanyiso-Msomi <nkanyisomsomi77@gmail.com>
2026-04-05 09:56:50 +0200

    Update Product.cpp

--- SYNNERS7.0/Product.cpp
+++ SYNNERS7.0/Product.cpp
@@ -58,34 +58,3 @@ void Product::increaseQuantity(int amount)
 
 
 
-//contructor of non perisharable
-NonPerishable::NonPerishable(int id, const std::string& productName, double price,
-                             double tax, int qty)
-    : Product(id, productName, price, qty),
-      taxRate(tax),{}
-
-//distructor of non perisharable
-NonPerishable::~NonPerishable() {
-
-}
-
-double NonPerishable::calculateFinalPrice() const {
-    double price = getPrice()+getPrice()*taxRate() ;
-
-
-    if (discountPrice!=0) {
-        price = getPrice()+getPrice()*taxRate() - discountPrice ;
-    }
-}
-
-//return tax rate
-double NonPerishable::getTaxRate() const {
-    return taxRate;
-}
-
-//update tax rate
-void NonPerishable::setTaxRate(double tax) {
-
-   taxRate = tax;
-
-}
