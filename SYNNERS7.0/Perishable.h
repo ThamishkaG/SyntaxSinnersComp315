6b68748ee6aee16af204448f8efb955e6cf90f8d
Nkanyiso-Msomi <nkanyisomsomi77@gmail.com>
2026-04-05 09:58:18 +0200

    Update Perishable.h

--- SYNNERS7.0/Perishable.h
+++ SYNNERS7.0/Perishable.h
@@ -4,17 +4,33 @@
 #include <Product.h>
 using namespace std;
 
-class Perishable : public Product
-{
-    private:
-    string expiryDate;
-    public:
-        Perishable(int productID,string name,double price,int quantity,double taxRate,double discountRate,string expiryDate);
-        virtual ~Perishable();
-
-
-    double calculateFinalPrice();
-    string getExpiryDate();
+class Perishable : public Product {
+private:
+    std::string expiryDate; 
+    double discountPercentage; 
+    
+public:
+   // Constructor: Initializes Perishable with given values
+    Perishable(int id, const std::string& productName, double price, 
+               const std::string& expiry, double discount, int qty );
+    
+    // distructor
+    ~Perishable();
+    
+    
+    double calculateFinalPrice() const override;
+    
+    
+    
+    // return the expiry date of a perishable product
+    std::string getExpiryDate() const;
+    
+    //update the expirery date 
+    void setExpiryDate(const std::string& expiry);
+    
+    //set a discount of a product 
+    void setDiscount(double discount);
+    
+  
 };
-
 #endif // PERISHABLE_H
